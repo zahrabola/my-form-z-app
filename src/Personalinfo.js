246390ca52d51d3fd58b22dceabdcffb16866efa
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-08-23 12:17:24 +0100

    Update
    personal and signup info

--- src/Personalinfo.js
+++ src/Personalinfo.js
@@ -4,6 +4,9 @@ function Personalinfo() {
   return (
     <div className="P-info">
       <h1>Personal info</h1>
+      <input type="text" placeholder="First Name ...." />
+      <input type="text" placeholder="Last Name ...." />
+      <input type="text" placeholder="Username ...." />
     </div>
   );
 }
--- src/Signup.js
+++ src/Signup.js
@@ -4,6 +4,9 @@ function Signup() {
   return (
     <div className="P-info">
       <h1>Sign Up</h1>
+      <input type="text" placeholder="Email Address...." />
+      <input type="text" placeholder="Password ...." />
+      <input type="text" placeholder="Confirm Password ...." />
     </div>
   );
 }
