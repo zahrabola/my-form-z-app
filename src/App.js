d5baa5ce9a56f44bc42ee569fe34fcc87163502b
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-08-23 12:00:13 +0100

    Update Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
    <h1>Zahra Olanrewaju </h1>
+   <Form /> 
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -4,6 +4,20 @@ function Form() {
   return (
     <div className="form">
       <h1>form </h1>
+      <div className="progressionbar">
+        <div
+          style={{ width: page === 0 ? "33.3%" : page == 1 ? "66.6%" : "100%" }}
+        ></div>
+
+        <div className="form-container">
+          <div className="header">Title</div>
+          <div className="body">Body info</div>
+          <div className="footer">
+            <button>Prev </button>
+            <button>Next </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
